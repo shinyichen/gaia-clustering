debd208b9952c50573bb6cab12735a4e29aef661
Xi Jin <fate_stigma@hotmail.com>
2018-09-26 19:44:44 -0700

    Refactor min hash

--- multi_layer_network/src/minhash2.py
+++ multi_layer_network/src/minhash2.py
@@ -1,6 +1,4 @@
-# Python function to print permutations of a given list
 import random
-import json
 import networkx as nx
 import jellyfish as jf
 import enchant
@@ -8,52 +6,57 @@ import enchant
 d = enchant.Dict("en_US")
 
 
-def getminHash(word, seed):
-    random.seed(seed)
-    word_dict = {}
-    for i in range(26):
-        word_dict[i] = chr(i + ord('a'))
-    for i in range(ord('\u052f') - ord('\u0400')):
-        word_dict[26 + i] = chr(i + ord('\u0400'))
-    all_list = list(range(26 + ord('\u052f') - ord('\u0400')))
-    hash_code = 0
-    while len(all_list) > 0:
-        idx = random.randint(0, len(all_list) - 1)
-        if word_dict[all_list[idx]] in word:
-            return hash_code
-        all_list[idx] = all_list[len(all_list) - 1]
-        all_list = all_list[:len(all_list) - 1]
-        hash_code += 1
-    return len(word_dict)
+class Blocking:
+    def __init__(self, seeds, weights):
+        self.seeds = seeds
+        self.weights = weights
+        self.word_dict = {}
+        for i in range(26):
+            self.word_dict[i] = chr(i + ord('a'))
+        for i in range(ord('\u052f') - ord('\u0400')):
+            self.word_dict[26 + i] = chr(i + ord('\u0400'))
 
-
-def get_blocking(cluster_heads, IDs, seed1, seed2, transDict, bn_prefix):
-    blocks = {}
-    print(len(IDs))
-    count = 0
-    for id1 in IDs:
-        count += 1
-        if count % 10000 == 0:
-            print(count * 1.0 / len(IDs))
-        word = cluster_heads[id1][0]
-        block_name = bn_prefix + str(getminHash(word, seed1) * 100 + getminHash(word, seed2) * 1)
-        if block_name not in blocks:
-            blocks[block_name] = []
-        blocks[block_name].append(id1)
-    return blocks
-
-
-def get_blocking_prefix(cluster_heads, IDs, transDict):
-    blocks = {}
-    for id1 in IDs:
-        word = cluster_heads[id1][0]
-        can = word.split(" ")
-        for i in can:
-            block_name = i.lower()[:2]
+    def get_blocking(self, cluster_heads, IDs, bn_prefix):
+        blocks = {}
+        print(len(IDs))
+        count = 0
+        for id1 in IDs:
+            count += 1
+            if count % 10000 == 0:
+                print(count * 1.0 / len(IDs))
+            word = cluster_heads[id1][0]
+            block_name = bn_prefix + str(sum([self.__get_min_hashing(word, seed) * weight
+                                              for seed, weight in zip(self.seeds, self.weights)]))
             if block_name not in blocks:
                 blocks[block_name] = []
             blocks[block_name].append(id1)
-    return blocks
+        return blocks
+
+    def __get_min_hashing(self, word, seed):
+        random.seed(seed)
+        all_list = list(range(len(self.word_dict)))
+        hash_code = 0
+        while len(all_list) > 0:
+            idx = random.randint(0, len(all_list) - 1)
+            if self.word_dict[all_list[idx]] in word:
+                return hash_code
+            all_list[idx] = all_list[len(all_list) - 1]
+            all_list = all_list[:len(all_list) - 1]
+            hash_code += 1
+        return len(self.word_dict)
+
+    @staticmethod
+    def get_blocking_prefix(cluster_heads, IDs):
+        blocks = {}
+        for id1 in IDs:
+            word = cluster_heads[id1][0]
+            can = word.split(" ")
+            for i in can:
+                block_name = i.lower()[:2]
+                if block_name not in blocks:
+                    blocks[block_name] = []
+                blocks[block_name].append(id1)
+        return blocks
 
 
 def same_index(cluster_heads, IDs):
@@ -69,8 +72,6 @@ def same_index(cluster_heads, IDs):
 
 
 def get_links_edge_list(cluster_heads):
-    transDict = {}
-    set_adding = set()
     IDs = list(cluster_heads.keys())
 
     G = nx.Graph()
@@ -84,7 +85,8 @@ def get_links_edge_list(cluster_heads):
             G.add_edge(sid[id][i], sid[id][i + 1])
 
     for ii in range(1, 3):
-        block1 = get_blocking(cluster_heads, IDs, ii, ii * 19, transDict, "first")
+        blocking = Blocking([ii, ii*19], [100, 1])
+        block1 = blocking.get_blocking(cluster_heads, IDs, "first")
         print("phase1_" + str(ii))
         count = 0
         print(len(block1))
@@ -113,7 +115,7 @@ def get_links_edge_list(cluster_heads):
                             if score > 0.9:
                                 G.add_edge(id1, id2)
         print(sum)
-    block1 = get_blocking_prefix(cluster_heads, IDs, transDict)
+    block1 = Blocking.get_blocking_prefix(cluster_heads, IDs)
     count = 0
     print(len(block1))
     sum = 0
@@ -136,8 +138,7 @@ def get_links_edge_list(cluster_heads):
                             score = 0
                         else:
                             score = jf.jaro_distance(name1, name2)
-                            if (name1[0].upper() != name1[0] or name2[0].upper() != name2[0] or d.check(
-                                    name1.lower()) or d.check(name2.lower())):
+                            if (name1[0].upper() != name1[0] or name2[0].upper() != name2[0] or d.check(name1.lower()) or d.check(name2.lower())):
                                 continue
                         if score > 0.9:
                             G.add_edge(id1, id2)
